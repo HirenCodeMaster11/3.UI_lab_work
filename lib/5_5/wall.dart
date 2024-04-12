f5bf6faf0ede90ac9e68c6e26cadcdd5e491eff1
HELLO <hirenbambhaniya120@gmail.com>
2024-04-12 18:47:43 +0530

    12-4-24

---
+++ lib/5_5/wall.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import 'wall_udf.dart';
+
+void main() {
+  runApp(dynamicList());
+}
+
+class dynamicList extends StatelessWidget {
+  const dynamicList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: homePage(),
+    );
+  }
+}
+
+class homePage extends StatefulWidget {
+  const homePage({super.key});
+
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: const Color(0xff212121),
+          centerTitle: true,
+          title: const Text(
+            'THE WALL',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+            Row1(),
+            Row2(),
+            Row1(),
+            Row2(),
+            Row1(),
+            Row2(),
+            Row1(),
+            Row2(),
+          ],
+        ),
+      ),
+    );
+  }
+}
